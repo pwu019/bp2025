7005b7c89f749b43d5cd75ce7f3af50c0671754e
Photini Wu <pwu019@gmail.com>
2025-03-01 17:50:05 -0500

    my dog generator

---
+++ index.js
@@ -0,0 +1,25 @@
+function getRandomDog() {
+    fetch("https://dog.ceo/api/breeds/image/random").then((res) => {
+        console.log(res);
+        return res.json();
+    }).then((data) => {
+        console.log(data);
+        document.querySelector("#dog-image").src = data.message;
+    });
+}
+
+function getSpecificDog () {
+    let breed = document.querySelector("#breed").value;
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`).then((res) => {
+        if (!res.ok) {
+            // an error has happened; we should do something!
+            alert("Woof, we couldn't find that dog :( Try again!");
+            return;
+        }
+        console.log(res);
+        return res.json();
+    }).then((data) => {
+        console.log(data);
+        document.querySelector("#dog-image").src = data.message;
+    });
+}
